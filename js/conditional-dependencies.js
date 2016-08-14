44a7dacc03c8927678e06f8cb18dc224ea2ad92e
H-Plus-Time <hydrogenplustime@gmail.com>
2016-08-15 08:40:04 +1000

    Minor tweaks, fixed display problem on safari

---
+++ js/conditional-dependencies.js
@@ -0,0 +1,10 @@
+if(!Modernizr.fetch) {
+	document.write('<script src="/bower_components\/fetch\/fetch.js"></script>')
+}
+if(!(Modernizr.template && Modernizr.htmlimports && 'registerElement' in document)) {
+	document.write('<script src="/bower_components\/webcomponentsjs\/webcomponents-lite.min.js"></script>')
+}
+
+if(!window.fetch) {
+	document.write('<script src="/bower_components/fetch/fetch.js"></script>')
+}
