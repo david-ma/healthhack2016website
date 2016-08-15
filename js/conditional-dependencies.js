7bd23c3cd9890537589a1ac224329ee31d0030ee
H-Plus-Time <hydrogenplustime@gmail.com>
2016-08-15 10:40:29 +1000

    removed modernizr (PITA)

--- js/conditional-dependencies.js
+++ js/conditional-dependencies.js
@@ -1,5 +1,10 @@
-if(!(Modernizr.template && Modernizr.htmlimports && 'registerElement' in document)) {
-	document.write('<script src="/bower_components\/webcomponentsjs\/webcomponents-lite.min.js"></script>')
+if ('registerElement' in document
+	&& 'createShadowRoot' in HTMLElement.prototype
+	&& 'import' in document.createElement('link')
+	&& 'content' in document.createElement('template')) {
+// We're using a browser with native WC support!
+} else {
+	document.write('<script src="/bower_components/webcomponentsjs/webcomponents-lite.js"><\/script>');
 }
 
 if(!window.fetch) {
