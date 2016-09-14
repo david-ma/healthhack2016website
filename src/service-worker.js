9656d7024a02ded6b0a47259ed1155cb05bdf0b3
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2016-09-14 18:32:39 +1000

    Tweaks to sponsors, challenges page

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,7 +5,7 @@ toolbox.router.get('js/*.js', toolbox.cacheFirst);
 toolbox.router.get('css/*.css', toolbox.cacheFirst);
 toolbox.router.get('*.html', toolbox.cacheFirst);
 toolbox.router.get('images/*', toolbox.cacheFirst);
-toolbox.router.get('bower_components/**/*', toolbox.cacheFirst);
+toolbox.router.get('bower_components/**/*', toolbox.fastest);
 toolbox.router.get('data/challenges.json', toolbox.cacheFirst);
 toolbox.router.get('/(.*)', toolbox.cacheFirst, {origin: 'http://cdn-images.mailchimp.com'});
 toolbox.router.get('/(.*)', toolbox.cacheFirst, {origin: 'http://s3.amazonaws.com'});
