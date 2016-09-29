dc93522cb647eba8f69b345ea2cc8f7f91935578
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2016-09-29 13:11:20 +1000

    Added sponsors section to index

---
+++ src/js/sponsors.js
@@ -0,0 +1,38 @@
+var sponsorsDiv = document.querySelector('#sponsors').querySelector('div > div >div');
+var sponsorTemplate = document.querySelector('#sponsor-template');
+function enumerateSponsorshipScope(scope) {
+  switch(scope) {
+    case 'national':
+      return 2;
+    case 'local':
+      return 1;
+    default:
+      return -1;
+  }
+}
+
+function enumerateLevel(level) {
+  switch(level) {
+    case 'gold':
+      return 2;
+    case 'silver':
+      return 1;
+    case 'community':
+      return 0;
+  }
+}
+fetch('/sponsors')
+.then(function(resp) { return resp.json()})
+.then(function(sponsors) {
+  return sponsors.sort(function(a,b) {
+    return a.priority - b.priority;
+  })
+})
+.then(function(sponsors) {
+  console.log(sponsors);
+  sponsors.forEach(function(sponsor) {
+    var sponsorInst = $.clone(sponsorTemplate);
+    sponsorInst.querySelector('img').src= sponsor.logo;
+    sponsorsDiv.appendChild(sponsorInst);
+  });
+})
