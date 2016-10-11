a0ff3720d5e38172f0adf62d06314c4477f2c2e7
Adam Woods <adam.tomato@gmail.com>
2016-10-11 12:22:57 +0800

    added external resources for conditional dependencies in an attempt to fix the challenges page

--- src/js/conditional-dependencies.js
+++ src/js/conditional-dependencies.js
@@ -1,11 +1,11 @@
 if ('registerElement' in document && 'createShadowRoot' in HTMLElement.prototype && 'import' in document.createElement('link') && 'content' in document.createElement('template')) {
 // We're using a browser with native WC support!
 } else {
-	document.write('<script src="/bower_components/es6-shim/es6-shim.min.js"></script>');
-	document.write('<script src="/bower_components/webcomponentsjs/webcomponents-lite.min.js"><\/script>');
+	document.write('<script src="https://bowercdn.net/c/es6-shim-0.35.1/es6-shim.min.js"></script>');
+	document.write('<script src="https://bowercdn.net/c/webcomponentsjs-0.7.22/webcomponents-lite.min.js"><\/script>');
 
 }
 
 if(!window.fetch) {
-	document.write('<script src="/bower_components/fetch/fetch.js"></script>');
+	document.write('<script src="https://bowercdn.net/c/fetch-1.0.0/fetch.js"></script>');
 }
